08e8360d9c27e049176a52d8fb2854a4a4d08043
25yzhang <25yzhang@derryfield.org>
2025-02-04 00:17:13 -0500

    HW28 Intro to Inheritance

---
+++ HW28/InheritDemo.java
@@ -0,0 +1,10 @@
+import zoo.*;
+
+public class InheritDemo {
+    public static void main(String[] args) {
+
+        Bovine bessy = new Bovine("Bessy", 1100);
+        bessy.feeedHay(100);
+        System.out.println("pounds of hay = " + bessy.getHayConsumed);
+    }
+}
