0455513be1fcf65d397dd3825f97fa55e697e937
25yzhang <25yzhang@derryfield.org>
2025-03-09 20:45:12 -0400

    Lesson A02 - Images and Animation

--- HWA02/animate/Board.java
+++ HWA02/animate/Board.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -14,8 +12,8 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Board extends JPanel {
-    private final int B_WIDTH = 350;
-    private final int B_HEIGHT = 350;
+    private final int B_WIDTH = 300;
+    private final int B_HEIGHT = 300;
     private BufferedImage img;
 
     // Constructor
@@ -23,7 +21,7 @@ public class Board extends JPanel {
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
 
         try {
-            File imageFile = new File("media/cakes.jpg");
+            File imageFile = new File("media/derryfield.jpg");
             img = ImageIO.read(imageFile);
             setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
         } catch (Exception e) {
@@ -38,26 +36,16 @@ public class Board extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
 
         AffineTransform affineTransform = new AffineTransform();
-        
+
         int x_t = 0, y_t = 0;
-        int x_scaled = 0, y_scaled = 0;
         double scale = 0.5;
-        if (img != null) {
-            x_t = this.getWidth() / 2;
-            x_scaled = (int) ((img.getWidth() * scale) / 2.0);
-            y_t = this.getHeight() / 2;
-            y_scaled = (int) ((img.getHeight() * scale) / 2.0);
-            affineTransform.translate(x_t - x_scaled, y_t - y_scaled);
-            affineTransform.rotate(Math.toRadians(45), x_scaled, y_scaled);
-            affineTransform.scale(scale, scale);
-            g2d.drawImage(img, affineTransform, null);
-        } else {
-            g2d.setColor(Color.BLUE);
-            g2d.drawString("Unable to load image!", 25, 25);
-        }
 
         if (img != null) {
-            g2d.drawImage(img, null, null);
+            x_t = this.getWidth() / 2;
+            y_t = this.getHeight() / 2;
+            affineTransform.translate(img.getWidth() / 2, img.getHeight() / 2);
+            affineTransform.scale(scale, scale);
+            g2d.drawImage(img, affineTransform, null);
         } else {
             g2d.setColor(Color.BLUE);
             g2d.drawString("Unable to load image!", 25, 25);
